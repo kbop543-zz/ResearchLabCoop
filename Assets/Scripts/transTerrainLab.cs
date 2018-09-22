54ac015f3d156534045eec98fa9c33aeb8415aca
basictroll <shens494@hotmail.com>
2018-09-22 15:33:51 -0400

    -added translocation script

---
+++ Assets/Scripts/transTerrainLab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transTerrainLab : MonoBehaviour {
+
+    //public GameObject player;
+    public Vector3 translocation;
+	
+	void OnCollisionEnter(Collision col) 
+    {
+        translocate(col.gameObject);
+    }
+
+    public void translocate(GameObject col)
+    {
+        col.transform.position += translocation;
+    }
+}
