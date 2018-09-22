7b29a694c110ac6c5bfee135416963794ff9d5c1
lance-kim <35354562+lance-kim@users.noreply.github.com>
2018-09-22 13:50:20 -0400

    Add files via upload

---
+++ Assets/Scripts/p2_movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2_movement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("P2 Horizontal");
+        float moveVertical = Input.GetAxis("P2 Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
