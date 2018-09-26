428dcfac9cfc5f4b58f2d80715c7ccbc7e178ce4
Kyra Stephen <kstephen852@hotmail.com>
2018-09-26 13:36:44 -0400

    added sound

---
+++ Assets/BackgroundSoundScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSoundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+	}
+}
