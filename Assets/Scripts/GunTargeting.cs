62147aa74d7eea84a9425527a39072e8b35192ea
Isabel Tang <isabeltang220@gmail.com>
2018-11-10 23:25:01 -0800

    Added gun targeting UI that shows where the player will shoot.

---
+++ Assets/Scripts/GunTargeting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunTargeting : MonoBehaviour {
+
+    public Canvas gunTargetUI;
+    public GameObject playerModel;
+
+	// Use this for initialization
+	void Start () {
+        gunTargetUI.transform.rotation = playerModel.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gunTargetUI.transform.rotation = playerModel.transform.rotation;
+    }
+}
