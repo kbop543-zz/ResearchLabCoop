ced7089410f7edd6ece5d135dede0d8bb9af2182
Sahil Gadhok <sahilgadhok@hotmail.com>
2018-11-14 15:16:14 -0500

    click sound on main menu

---
+++ Assets/audio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class audio : MonoBehaviour {
+
+    public AudioSource clickSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.JoystickButton0)){
+            clickSound.Play();
+        }
+	}
+}
